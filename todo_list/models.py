1830e579ae65daed166adc6681922f5eb9cf4e90
karngyan <karngyan@gmail.com>
2019-04-03 11:54:39 +0530

    Date Time Added

--- todo_list/models.py
+++ todo_list/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 class List(models.Model):
 	item = models.CharField(max_length=200)
 	completed = models.BooleanField(default=False)
+	date_time = models.CharField(max_length=200)
 
 	def __str__(self):
-		return self.item + ' | ' + str(self.completed)
+		return self.item + ' | ' + str(self.completed) + ' | ' + str(self.date_time)
--- todo_list/views.py
+++ todo_list/views.py
@@ -3,19 +3,25 @@ from .models import List
 from .forms import ListForm
 from django.contrib import messages
 
+from datetime import datetime
+
 def home(request):
+
+	my_date = datetime.now()
+
 	if request.method == 'POST':
 		form = ListForm(request.POST or None)
+		all_items = List.objects.all
 
 		if form.is_valid():
 			form.save()
 			all_items = List.objects.all
 			messages.success(request , ('Item Has Been Added To List!'))
-			return render(request , 'home.html' , {'all_items': all_items})
+		return render(request , 'home.html' , {'all_items': all_items , 'my_date': my_date})
 		
 	else:
 		all_items = List.objects.all
-		return render(request , 'home.html' , {'all_items': all_items})
+		return render(request , 'home.html' , {'all_items': all_items , 'my_date': my_date})
 
 def delete(request , list_id):
 	item = List.objects.get(pk=list_id)
